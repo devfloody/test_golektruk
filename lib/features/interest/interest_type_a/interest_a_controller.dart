d773ae2ccd7c6a76a9b44e35b2956dce4d49494e
Fahmi Ismail <fahmiismail353@gmail.com>
2023-01-14 10:38:58 +0700

    fix hitung bunga tyoe A

--- lib/features/interest/interest_type_a/interest_a_controller.dart
+++ lib/features/interest/interest_type_a/interest_a_controller.dart
@@ -7,32 +7,30 @@ class InterestTypeANotifier extends StateNotifier<List<double>> {
     double rate;
     double admin;
     double interest;
+    state = state..clear();
     for (int i = 1; i <= months; i++) {
       if (principal >= 100000 && principal <= 500000) {
         rate = 0.02;
         admin = 1500;
         interest = (principal * rate);
         principal = principal + interest - admin;
-        print("Month $i: Total savings = $principal");
       } else if (principal > 500000 && principal <= 10000000) {
         rate = 0.03;
         admin = 2000;
         interest = (principal * rate);
         principal = principal + interest - admin;
-        print("Month $i: Total savings = $principal");
       } else if (principal > 10000000 && principal <= 50000000) {
         rate = 0.04;
         admin = 2500;
         interest = (principal * rate);
         principal = principal + interest - admin;
-        print("Month $i: Total savings = $principal");
       } else if (principal > 50000000) {
         rate = 0.05;
         admin = 5000;
         interest = (principal * rate);
         principal = principal + interest - admin;
-        print("Month $i: Total savings = $principal");
       }
+      state = state..add(principal);
     }
   }
 }
--- lib/features/interest/interest_type_a/interest_type_a.dart
+++ lib/features/interest/interest_type_a/interest_type_a.dart
@@ -1,6 +1,9 @@
+import 'dart:developer';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:intl/intl.dart';
 import 'package:test_golektruk/features/interest/interest_type_a/interest_a_controller.dart';
 
 import '../../../utils/fonts/app_font.dart';
@@ -12,6 +15,7 @@ class InterestTypeA extends HookConsumerWidget {
     final principalCtrl = useTextEditingController();
     const months = 6;
     final principal = ref.watch(interestTypeAProvider);
+    final formKey = GlobalKey<FormState>();
 
     return Scaffold(
       appBar: AppBar(
@@ -24,58 +28,96 @@ class InterestTypeA extends HookConsumerWidget {
       ),
       body: Padding(
         padding: const EdgeInsets.all(20.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            TextFormField(
-              controller: principalCtrl,
-              keyboardType: TextInputType.number,
-              textAlign: TextAlign.center,
-              decoration: InputDecoration(
-                hintText: "Masukkan Jumlah Tabungan",
-                hintStyle: AppFont.poppinsRegular.copyWith(fontSize: 14),
-                labelText: "Jumlah Tabungan",
-                labelStyle: AppFont.poppinsMedium,
-              ),
-            ),
-            // ListView.builder(
-            //   itemCount: months,
-            //   shrinkWrap: true,
-            //   itemBuilder: (context, index) {
-            //     final priceInRupiah = NumberFormat.currency(
-            //             locale: 'id', decimalDigits: 2, symbol: 'Rp ')
-            //         .format(principal);
-            //     return Text("Bulan ke $index = ${principal[index]}");
-            //   },
-            // ),
-            SizedBox(
-              width: double.infinity,
-              height: 48,
-              child: ElevatedButton(
-                onPressed: () {
-                  if (principalCtrl.text.isNotEmpty) {
-                    if (double.parse(principalCtrl.text) >= 100000.0) {
-                      ref.read(interestTypeAProvider.notifier).hitungBunga(
-                            principal: double.parse(principalCtrl.text),
-                            months: months,
-                          );
-                    } else {
-                      ScaffoldMessenger.of(context).showSnackBar(
-                        const SnackBar(
-                          content: Text("Jumlah Tabungan dibawah ketentuan"),
-                        ),
-                      );
-                    }
+        child: Form(
+          key: formKey,
+          child: Column(
+            mainAxisSize: MainAxisSize.max,
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              TextFormField(
+                controller: principalCtrl,
+                keyboardType: TextInputType.number,
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return "Jumlah tabungan harus diisi";
                   }
+                  return null;
                 },
-                child: Text(
-                  "Hitung Bunga",
-                  style: AppFont.poppinsSemiBold.copyWith(fontSize: 14),
+                textAlign: TextAlign.center,
+                decoration: InputDecoration(
+                  hintText: "Masukkan Jumlah Tabungan",
+                  hintStyle: AppFont.poppinsRegular.copyWith(fontSize: 14),
+                  labelText: "Jumlah Tabungan",
+                  labelStyle: AppFont.poppinsMedium,
                 ),
               ),
-            )
-          ],
+              SizedBox(
+                width: double.infinity,
+                height: 48,
+                child: ElevatedButton(
+                  onPressed: () {
+                    formKey.currentState!.validate();
+                    if (principalCtrl.text.isNotEmpty) {
+                      if (double.parse(principalCtrl.text) >= 100000.0) {
+                        ref.read(interestTypeAProvider.notifier).hitungBunga(
+                              principal: double.parse(principalCtrl.text),
+                              months: months,
+                            );
+                        log(principal.toString());
+                        showDialog(
+                          context: context,
+                          builder: (context) {
+                            return SimpleDialog(
+                              contentPadding: const EdgeInsets.symmetric(
+                                  horizontal: 24, vertical: 16),
+                              title: const Text("Bunga selama 6 bulan"),
+                              titleTextStyle: AppFont.poppinsMedium.copyWith(
+                                fontSize: 18,
+                                color: Colors.black,
+                              ),
+                              children: [
+                                SizedBox(
+                                  height: 160,
+                                  child: ListView.builder(
+                                    itemCount: principal.length,
+                                    itemBuilder: (context, index) {
+                                      final investPerMonth = principal[index];
+                                      final idrFormat = NumberFormat.currency(
+                                          locale: 'id',
+                                          symbol: 'Rp. ',
+                                          decimalDigits: 2);
+                                      final investInIdr =
+                                          idrFormat.format(investPerMonth);
+                                      return Row(
+                                        children: [
+                                          Text("Bulan ke $index : "),
+                                          Text(investInIdr),
+                                        ],
+                                      );
+                                    },
+                                  ),
+                                ),
+                              ],
+                            );
+                          },
+                        );
+                      } else {
+                        ScaffoldMessenger.of(context).showSnackBar(
+                          const SnackBar(
+                            content: Text("Jumlah Tabungan dibawah ketentuan"),
+                          ),
+                        );
+                      }
+                    }
+                  },
+                  child: Text(
+                    "Hitung Bunga",
+                    style: AppFont.poppinsSemiBold.copyWith(fontSize: 14),
+                  ),
+                ),
+              )
+            ],
+          ),
         ),
       ),
     );
