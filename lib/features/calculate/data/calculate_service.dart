fa97eb1bb2a35caece28daa4ac7097682965dbf6
Fahmi Ismail <fahmiismail353@gmail.com>
2023-01-15 22:59:41 +0700

    create calculatemodel

--- lib/features/calculate/data/calculate_service.dart
+++ lib/features/calculate/data/calculate_service.dart
@@ -1,33 +1,7 @@
 import 'package:test_golektruk/features/calculate/domain/calculate_model.dart';
 
 class CalculateService {
-  // tambah({
-  //   required CalculateModel calculateModel,
-  //   required int inputNumber,
-  // }) {
-  //   if (calculateModel.isChecked) {
-  //     return calculateModel.value + inputNumber;
-  //   }
-  // }
-
-  // kurang({
-  //   required CalculateModel calculateModel,
-  //   required int inputNumber,
-  // }) {
-  //   if (calculateModel.isChecked) {
-  //     return calculateModel.value - inputNumber;
-  //   }
-  // }
-
-  // bagi({
-  //   required CalculateModel calculateModel,
-  //   required int inputNumber,
-  // }) {
-  //   if (calculateModel.isChecked) {
-  //     if (calculateModel.value != 0) {
-  //       return calculateModel.value / inputNumber;
-  //     }
-  //   }
-  // }
-  
+  calculate(List<CalculateModel> numbers, String operation) {
+    
+  }
 }
--- lib/features/calculate/domain/calculate_model.dart
+++ lib/features/calculate/domain/calculate_model.dart
@@ -1,35 +1,11 @@
-import 'dart:convert';
-
 class CalculateModel {
-  final double value;
+  final double? value;
   final bool? isChecked;
 
   CalculateModel({
-    required this.value,
-    required this.isChecked,
+    this.value,
+    this.isChecked,
   });
-
-  Map<String, dynamic> toMap() {
-    return {
-      'value': value,
-      'isChecked': isChecked,
-    };
-  }
-
-  factory CalculateModel.fromMap(Map<String, dynamic> map) {
-    return CalculateModel(
-      value: double.tryParse(map['value']) ?? 0,
-      isChecked: map['isChecked'] ?? false,
-    );
-  }
-
-  String toJson() => json.encode(toMap());
-
-  factory CalculateModel.fromJson(String source) =>
-      CalculateModel.fromMap(json.decode(source));
-
-  @override
-  String toString() => 'CalculateModel(value: $value, isChecked: $isChecked)';
 
   CalculateModel copyWith({
     double? value,
@@ -40,4 +16,7 @@ class CalculateModel {
       isChecked: isChecked ?? this.isChecked,
     );
   }
+
+  @override
+  String toString() => 'CalculateModel(value: $value, isChecked: $isChecked)';
 }
